350f368a4c357aafbca9f2811983605a60e62c03
Aniket-Sharma99 <51778272+Aniket-Sharma99@users.noreply.github.com>
2019-06-13 18:26:44 +0530

    Update views.py

--- ORM/testapp/views.py
+++ ORM/testapp/views.py
@@ -3,6 +3,6 @@ from testapp.models import employee
 from django.db.models.functions import Lower
 
 def display(request):
-    e1=employee.Aniket.get_queryset1()     #objects is not necessary we can write anything but then change in models.py
+    e1=employee.objects.get_queryset1()     #objects is not necessary we can write anything but then change in models.py
     dict={'e1':e1}
     return render(request,"Testapp/list.html",context=dict)
