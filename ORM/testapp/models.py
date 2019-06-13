a47ec2d4d895bc9e9fded69bd55e80a73fa9c616
Aniket-sharma99 <sharmaaniketskit99@gmail.com>
2019-06-13 17:20:14 +0530

    commit first time

---
+++ ORM/testapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class customManager(models.Manager):
+    def get_queryset1(self):
+        return super().get_queryset().order_by('esal')
+    def get_emp_range(self,range1,range2):
+        return super().get_queryset().filter(esal__range=(range1,range2)).order_by('esal')
+    def get_emp_sorted(self,param):
+        return super().get_queryset().order_by(param)
+
+class employee(models.Model):
+    ename=models.CharField(max_length=20)
+    eno=models.IntegerField()
+    esal=models.IntegerField()
+    ecity=models.CharField(max_length=20)
+    Aniket=customManager()
---
+++ ORM/testapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from testapp.models import employee
+from django.db.models.functions import Lower
+
+def display(request):
+    e1=employee.Aniket.get_queryset1()     #objects is not necessary we can write anything but then change in models.py
+    dict={'e1':e1}
+    return render(request,"Testapp/list.html",context=dict)
